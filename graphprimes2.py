5cb4326fadbd903e004fc8740c258ca5b01b5953
Nick Hall <nick@vaneckzero.com>
2023-05-10 17:04:53 -0500

    experimenting with graphing prime and their gaps

---
+++ graphprimes2.py
@@ -0,0 +1,28 @@
+import re
+import matplotlib.pyplot as plt
+
+def extract_data(filename):
+    with open(filename, 'r') as file:
+        content = file.readlines()
+
+    primes = []
+    gaps = []
+
+    for line in content:
+        prime, gap = re.findall(r'\d+', line)
+        primes.append(int(prime))
+        gaps.append(int(gap))
+
+    return primes, gaps
+
+def plot_data(primes, gaps):
+    plt.plot(primes, gaps, marker='o', linestyle='-', linewidth=1)
+    plt.xlabel('Prime Numbers')
+    plt.ylabel('Prime Gaps')
+    plt.title('Prime Numbers vs Prime Gaps')
+    plt.show()
+
+if __name__ == '__main__':
+    filename = 'primelist.txt'  # Replace with your input file name
+    primes, gaps = extract_data(filename)
+    plot_data(primes, gaps)
